3e7a63c394d1a6ccbd03e8cb30f7783d76310772
cat <ishu.cxx@gmail.com>
2025-05-16 16:34:21 +0530

    cat

---
+++ src/collections/Pages.ts
@@ -0,0 +1,44 @@
+import type { CollectionConfig } from 'payload'
+import { richText } from 'payload/shared'
+// import { lexicalEditor } from '@payloadcms/richtext-lexical'
+
+export const Pages: CollectionConfig = {
+  slug: 'pages',
+  admin: {
+    useAsTitle: 'title',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'featuredImage',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'published',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    {
+      name: 'publishDate',
+      type: 'date',
+    },
+    {
+      name: 'content',
+      type: 'richText',
+    },
+  ],
+}
