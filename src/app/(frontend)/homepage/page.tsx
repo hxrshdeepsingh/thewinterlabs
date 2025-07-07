71f2813c4da024b0e5a207c92afbcd80087d7ff3
igomeow <ishu.cxx@gmail.com>
2025-07-08 03:37:38 +0530

    meow

--- src/app/(frontend)/homepage/page.tsx
+++ src/app/(frontend)/homepage/page.tsx
@@ -3,13 +3,17 @@ import { HeroSection } from '@/components/hero-section-1'
 import { Footerdemo } from '@/components/ui/footer-section'
 import Testimonials from '@/components/testimonials'
 import FeatureStepsDemo from '@/components/feature-step'
+import { Gallery4 } from '@/components/gallery4'
+import { Footer2 } from '@/components/ui/shadcnblocks-com-footer2'
+
 export default function page() {
   return (
     <>
       <HeroSection />
+      <Gallery4 />
       <Testimonials />
       <FeatureStepsDemo />
-      <Footerdemo />
+      <Footer2 />
     </>
   )
 }
