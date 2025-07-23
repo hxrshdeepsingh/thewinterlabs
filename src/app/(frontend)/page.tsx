544a4c4c46f64a7000484a218528df91bee880fd
igomeow <ishu.cxx@gmail.com>
2025-07-24 02:44:23 +0530

    meow

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import * as React from 'react'
-import Hero from '@/components/Hero'
+import Hero from '../../components/Hero'
 import Testimonials from '@/components/Testimonials'
 import Gallery from '@/components/Gallery'
 
