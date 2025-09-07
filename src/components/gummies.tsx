7b8fcd4f80ec8b8d2788c16c8af8e29fec3459a3
igomeow <ishu.cxx@gmail.com>
2025-09-07 13:13:33 +0530

    production

--- src/components/gummies.tsx
+++ src/components/gummies.tsx
@@ -2,11 +2,9 @@ import Spline from '@splinetool/react-spline/next'
 
 export default function Gummies() {
   return (
-    <div className="center-spline">
+    <div className="center-spline hidden md:block">
       <Spline
         scene="https://prod.spline.design/DZ5MiYIB8R5VqKcJ/scene.splinecode"
-        width={635}
-        height={250}
       />
     </div>
   )
