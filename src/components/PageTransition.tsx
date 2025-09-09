e5e476c957c1082cbe9173db72190c5c3777142f
igomeow <ishu.cxx@gmail.com>
2025-09-09 12:34:28 +0530

    production

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -13,7 +13,7 @@ export default function PageTransition({ children }: { children: React.ReactNode
         initial={{ opacity: 0, filter: 'blur(10px)' }}
         animate={{ opacity: 1, filter: 'blur(0px)' }}
         exit={{ opacity: 0, filter: 'blur(10px)' }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
+        transition={{ duration: 0.2, ease: 'easeInOut' }}
       >
         {children}
       </motion.div>
